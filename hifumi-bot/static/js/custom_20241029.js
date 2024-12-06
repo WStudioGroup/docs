264259716f1592fa03b91ca2c72063064ce1f805
captain-wangrun-cn <18889281583@qq.com>
2024-12-07 02:39:27 +0800

    更新了表情包功能，修改了doc1首页，更换二维码

--- hifumi-bot/static/js/custom_20241029.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?5d1e509c6e1bba21101b1559f973e038";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
