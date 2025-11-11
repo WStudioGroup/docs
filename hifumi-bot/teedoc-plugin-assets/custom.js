5af2f17d7da1cd43017b6f6d3a245647e84bb804
Wang Run <wangrun114514@foxmail.com>
2025-11-11 21:44:26 +0800

    ..

--- hifumi-bot/teedoc-plugin-assets/custom.js
+++
@@ -1,8 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?dc96f70654d2b88928f758abce6e382d";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-("#Background").height($("#Background").height());
